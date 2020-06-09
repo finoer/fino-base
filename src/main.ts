7852806fde47e7c58fa54d8f349a07be5f557e49
wangdongxu <wangdongxu@vipkid.com.cn>
2020-06-09 14:45:21 +0800

    feat:  add ts references

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,17 @@
  * @author xxx
  * @since 2020.01.01
  */
-
-//import ba from '../../../packages/finoer-core/src'
-
-import hello from 'Packs/finoer-core/src'
-
+import Finoer from 'Packs/finoer-core'
 
 export default class Main {
     public static main(): void {
       console.log("ok")
     }
 }
-  
-  Main.main();
-   let a:hello = new hello()
- //  let bb:ba = new ba();
 
-   a.setName("132")
-   a.getName()
+
+Main.main();
+let fino:Finoer = new Finoer()
+console.log('fino', fino)
+fino.setName('yueqi')
+
