187f7518e48e4ce3dbcd51ee7975b93256062260
wangdongxu <wangdongxu@vipkid.com.cn>
2020-08-24 10:42:19 +0800

    set fino entry

---
+++ project.ts
@@ -0,0 +1,22 @@
+const projectList = [
+  {
+    name: 'yueqi1',
+    activeWhen: function (location: Location) {
+      const route= window.location.href.split(window.location.origin)[1]
+      return route.startsWith('/yueqi')
+    },
+    domain: "http://localhost:8080",
+    entry: '/rcms/stats.js',
+  },
+  {
+    name: 'jinzhao',
+    activeWhen: function (location: Location) {
+      const route= window.location.href.split(window.location.origin)[1]
+      return route.startsWith('/jinzhao')
+    },
+    domain: "http://localhost:8081",
+    entry: '/rcms/stats.js',
+  },
+]
+
+export default projectList
--- src/main.ts
+++ src/main.ts
@@ -2,29 +2,12 @@
  * @description example class
  * @author xxx
  * @since 2020.01.01
+ *
  */
-import Finoer from 'Packs/finoer-core'
-import { Main } from 'Packs/finoer-lib';
-import { Startup, HelloWorld } from 'Packs/package-startup'
+import { invoke, registerApps } from 'Packs/finoer-invoke'
+import projectList from '../project'
+
+// 注册应用
+registerApps(projectList)
 
 
-export default class Main1 {
-  public static main(): void {
-    console.log("ok")
-  }
-}
-
-
-Main1.main();
-let fino: Finoer = new Finoer()
-console.log('fino', fino)
-fino.setName('yueqi')
-
-let main: Main = new Main()
-console.log('main', main)
-
-let startup: Startup = new Startup();
-
-console.log('startup', startup)
-
-console.log('hellow world', HelloWorld)
