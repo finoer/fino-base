6509a07de302dfef6092dbe6adadb262aec1395b
wangdongxu <wangdongxu@vipkid.com.cn>
2021-01-26 14:35:42 +0800

    fix(build): modify the content of copy-webpack-plugin

--- build/devConfig/plugins.js
+++ build/devConfig/plugins.js
@@ -4,35 +4,28 @@ const resolve = require('../utils/resolve')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const devPlugins = [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"development"'
-      }
-    }),
-  	new CopyWebpackPlugin(
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"development"'
+    }
+  }),
+  new CopyWebpackPlugin(
 
-		{
-			patterns: [
-				{
-					from: resolve('src/img'),
-					to: resolve('dist'),
-					globOptions:{
-						ignore: ['.*']
-					}
-        },
+    {
+      patterns: [
         {
           from: resolve('src/static'),
           to: resolve('dist'),
-          globOptions:{
-						ignore: ['.*']
-					}
+          globOptions: {
+            ignore: ['.*']
+          }
         },
-			]
+      ]
 
-		}
+    }
 
-	)
+  )
 ]
 
 const pluginsConfig = [...commonPlugins, ...devPlugins]
